ebcc5d7013238d8cb714eba6e68a4bec87385fc8
nhancao30827 <nhancao03ct@gmail.com>
2025-12-18 13:50:56 +0700

    Init database

--- main.py
+++ main.py
@@ -1,6 +1,36 @@
-def main():
-    print("Hello from book-management!")
+from fastapi import FastAPI
+from sqlmodel import SQLModel
+from src.books.routes import book_router
+from contextlib import asynccontextmanager
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncEngine
+from src.config import settings
+
+engine: AsyncEngine = create_async_engine(
+    settings.DATABASE_URL,
+    echo=True
+)
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    print("Application is starting up...")
+    await init_db()
+    yield
+    print("Application is shutting down...")
 
 
-if __name__ == "__main__":
-    main()
+version = 'v1'
+
+app = FastAPI(
+    title='Bookly',
+    description='A RESTful API for a book review web service',
+    version=version,
+    lifespan=lifespan
+)
+
+app.include_router(book_router,prefix=f"/api/{version}/books", tags=['books'])
+
+async def init_db():
+    async with engine.begin() as conn:
+        from src.books.models import Book
+
+        await conn.run_sync(SQLModel.metadata.create_all)
---
+++ src/books/models.py
@@ -0,0 +1,32 @@
+from sqlmodel import SQLModel, Field, Column
+import sqlalchemy.dialects.postgresql as pg
+from datetime import datetime, date
+import uuid
+
+class Book(SQLModel, table=True):
+    __tablename__ = "books"
+
+    uid: uuid.UUID = Field(
+        sa_column=Column(
+            pg.UUID(as_uuid=True),
+            primary_key=True,
+            default=uuid.uuid4,
+        )
+    )
+    
+    title: str = Field(...,max_length=255, index=True)
+    author: str = Field(...,max_length=255)
+    publisher: str = Field(...,max_length=255)
+    published_date: date = Field(...)
+    page_count: int
+    language: str = Field(...,max_length=50)
+
+    created_at: datetime = Field(
+        sa_column=Column(pg.TIMESTAMP, default=datetime.utcnow)
+    )
+    updated_at: datetime = Field(
+        sa_column=Column(pg.TIMESTAMP, default=datetime.utcnow, onupdate=datetime.utcnow)
+    )
+
+    def __repr__(self) -> str:
+        return f"<Book {self.title}>"
